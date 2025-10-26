97031bff1d21c24e9074dc588544daa69b8692af
Anthony N  Albuquerque <anthonyhitnailbr@gmail.com>
2025-10-26 10:57:51 -0300

    variaves do css

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,11 @@ export function App() {
   console.log('App renderizou');
   return (
     <Fragment>
-      <Heading />
+      <Heading attr={123} attr2='String'>
+        Olá Mundo 1
+      </Heading>
+      <Heading> Olá Mundo 2</Heading>
+      <Heading> Olá Mundo 3</Heading>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque quia
         voluptatibus et magni ducimus? Illo soluta molestias provident quibusdam
