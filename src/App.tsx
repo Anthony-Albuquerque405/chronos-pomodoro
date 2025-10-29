d4d011155978532da78f919df0f5ddff495313df
Anthony N  Albuquerque <anthonyhitnailbr@gmail.com>
2025-10-29 07:16:45 -0300

    Organizando componentes em pastas padronizadas

--- src/App.tsx
+++ src/App.tsx
@@ -5,30 +5,19 @@
 import { Fragment } from 'react/jsx-runtime';
 import './styles/theme.css';
 import './styles/global.css';
+import { Heading } from './components/Heading';
+import { Conteiner } from './components/Conteiner';
 
 export function App() {
   return (
     <Fragment>
-      <div className='conteiner'>
-        <div className='conteudo'>
-          <section>LOGO</section>
-        </div>
-      </div>
-      <div className='conteiner'>
-        <div className='conteudo'>
-          <section>MENU</section>
-        </div>
-      </div>
-      <div className='conteiner'>
-        <div className='conteudo'>
-          <section>FORMULARIO</section>
-        </div>
-      </div>
-      <div className='conteiner'>
-        <div className='conteudo'>
-          <section>FOOTER</section>
-        </div>
-      </div>
+      <Conteiner>
+        <Heading>LOGO</Heading>
+      </Conteiner>
+
+      <Conteiner>
+        <Heading>MENU</Heading>
+      </Conteiner>
     </Fragment>
   );
 }
