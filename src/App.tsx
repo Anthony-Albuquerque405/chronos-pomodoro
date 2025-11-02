4b3cbefe013601f3091bc7331bfd4520f5c67ab3
Anthony N  Albuquerque <anthonyhitnailbr@gmail.com>
2025-11-01 22:22:00 -0300

    Criando o formulario

--- src/App.tsx
+++ src/App.tsx
@@ -25,6 +25,28 @@ export function App() {
       <Conteiner>
         <CountDown></CountDown>
       </Conteiner>
+
+      <Conteiner>
+        <form className='form' action=''>
+          <div className='formRow'>
+            <label htmlFor='meuInput'>task</label>
+            <input id='meuInput' type='text' />
+          </div>
+
+          <div className='formRow'>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+
+          <div className='formRow'>
+            <p>Ciclos</p>
+            <p>0 0 0 0 0 0 0</p>
+          </div>
+
+          <div className='formRow'>
+            <button>Enviar</button>
+          </div>
+        </form>
+      </Conteiner>
     </Fragment>
   );
 }
