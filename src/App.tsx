62c788e992ceceafd59001097b77fd8e2f073632
Anthony N  Albuquerque <anthonyhitnailbr@gmail.com>
2025-11-09 08:00:47 -0300

    Enviando o estado via props para varios componentes.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
 import './styles/theme.css';
 import './styles/global.css';
 import { Home } from './pages/Home';
+import { useState } from 'react';
+import type { TaskStateModel } from './models/TaskStateModel';
+
+const initialState: TaskStateModel = {
+  tasks: [],
+  secondsRemaining: 0,
+  formattedSecondsRaemining: '00:00',
+  activeTask: null,
+  currentCycle: 0,
+  config: {
+    workTime: 25,
+    shortBreakTime: 5,
+    longBreakTime: 15,
+  },
+};
 
 export function App() {
-  return <Home></Home>;
+  const [state, setState] = useState(initialState);
+
+  return <Home state={state} setState={setState}></Home>;
 }
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,17 +1,23 @@
 import { Conteiner } from '../../components/Conteiner';
 import { CountDown } from '../../components/CountDown';
 import { MainForm } from '../../components/MainForm';
+import type { TaskStateModel } from '../../models/TaskStateModel';
 import { MainTemplate } from '../../templates/MainTemplates';
 
-export function Home() {
+export type HomeProps = {
+  state: TaskStateModel;
+  setState: React.Dispatch<React.SetStateAction<TaskStateModel>>;
+};
+
+export function Home(props: HomeProps) {
   return (
     <MainTemplate>
       <Conteiner>
-        <CountDown></CountDown>
+        <CountDown {...props}></CountDown>
       </Conteiner>
 
       <Conteiner>
-        <MainForm></MainForm>
+        <MainForm {...props}></MainForm>
       </Conteiner>
     </MainTemplate>
   );
